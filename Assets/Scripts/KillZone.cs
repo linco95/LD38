6468aa1aa50e7cbdaecd35e346a81b4afc2c4e28
linco95 <linco95@gmail.com>
2017-04-23 21:58:13 +0200

    Made killzone working

--- Assets/Scripts/KillZone.cs
+++ Assets/Scripts/KillZone.cs
@@ -7,8 +7,7 @@ using UnityEngine;
 public class KillZone : MonoBehaviour {
     private void OnTriggerEnter2D(Collider2D collision) {
         if (collision.CompareTag("Player")) {
-            GameObject.FindGameObjectWithTag("Respawn");
-
+            collision.transform.position = GameObject.FindGameObjectWithTag("Respawn").transform.position;
         }
     }
 }
