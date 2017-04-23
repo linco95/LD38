46e80f358ecd0c77a7fb383f1415bd8443e891bd
linco95 <linco95@gmail.com>
2017-04-23 23:24:46 +0200

    Basic movement is now working properly

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -21,11 +21,25 @@ public class PlayerMovement : MonoBehaviour {
     }
 
     private bool isGrounded() {
-        RaycastHit2D[] hits = Physics2D.RaycastAll(transform.position, -transform.up, GroundRayLength);
-        Debug.DrawRay(transform.position, -transform.up.normalized * GroundRayLength);
-        foreach (RaycastHit2D hit in hits) {
-            if (hit.transform.CompareTag("Ground")) {
+        if (Math.Abs(rbody.velocity.y) > 0.5) return false;
+
+        GameObject groundcheck = transform.FindChild("GroundCheck").gameObject;
+        if (groundcheck != null) {
+            ContactFilter2D CF2D = new ContactFilter2D();
+            CF2D.SetLayerMask(LayerMask.GetMask("Ground"));
+            Collider2D[] colliders = new Collider2D[1];
+            if (groundcheck.GetComponent<Collider2D>().OverlapCollider(CF2D, colliders) > 0) {
                 return true;
+            }
+        }
+        else {
+            // Raycast if no groundcheck was found. This will only look in middle of character though
+            RaycastHit2D[] hits = Physics2D.RaycastAll(transform.position, -transform.up, GroundRayLength);
+            Debug.DrawRay(transform.position, -transform.up.normalized * GroundRayLength, Color.red);
+            foreach (RaycastHit2D hit in hits) {
+                if (hit.transform.CompareTag("Ground")) {
+                    return true;
+                }
             }
         }
         return false;
@@ -44,10 +58,11 @@ public class PlayerMovement : MonoBehaviour {
         }
 
         //Use the two store floats to create a new Vector2 variable movement.
-        Vector2 horizontalMovement = new Vector2(moveHorizontal, 0);
+        Vector2 horizontalMovement = new Vector2(moveHorizontal * maxSpeed * Time.fixedDeltaTime, rbody.velocity.y);
         Vector2 verticalMovement = new Vector2(0, moveVertical);
 
+        rbody.velocity = horizontalMovement;
         //Call the AddForce function of our Rigidbody2D rb2d supplying movement multiplied by speed to move our player.
-        rbody.AddForce(horizontalMovement * maxSpeed + verticalMovement * jumpVelocity);
+        rbody.AddForce(verticalMovement * jumpVelocity);
     }
 }
