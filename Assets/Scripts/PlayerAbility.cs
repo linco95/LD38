a511130f255f870aae9c13aac02b6f1ce4be21f4
linco95 <linco95@gmail.com>
2017-04-23 23:39:33 +0200

    Started working on player ability

---
+++ Assets/Scripts/PlayerAbility.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAbility : MonoBehaviour {
+
+
+    private void Update() {
+        if (Input.GetAxis("Fire1") > 0.0f) {
+            transform.localScale = new Vector2(2, 2);
+        }
+        else {
+            transform.localScale = new Vector2(1, 1);
+
+        }
+    }
+	
+}
---
+++ Assets/Scripts/WinLevel.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(Collider2D))]
+public class WinLevel : MonoBehaviour {
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.collider.CompareTag("Player")) {
+            SceneManager.LoadScene("GameWon");
+        }
+    }
+}
